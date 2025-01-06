f919ab2264dcbcd0966a8892eeb9ba2803d9761a
Raihan115219 <aburaihan115219@gmail.com>
2025-01-06 11:27:25 +0600

    not found page added

---
+++ redirect.js
@@ -0,0 +1,22 @@
+// Define valid routes
+const routes = [
+  "/", // Home
+  "/pages/about.html", // About Us
+  "/pages/service.html", // All Services
+  "/pages/web-development-services.html", // Web Development
+  "/pages/app-development-services.html", // App Development
+  "/pages/projects.html", // Portfolio
+  "/pages/contact.html", // Contact
+];
+
+// Get the current route
+const path = window.location.pathname;
+console.log("Current Path:", path);
+
+// Check if the path is valid
+if (!routes.includes(path)) {
+  console.log("Invalid path detected. Redirecting to 404...");
+  window.location.href = "/404.html";
+} else {
+  console.log("Valid path:", path);
+}
