87dc578dca377f47389bbaf7223cb3aa619acc12
Raihan115219 <aburaihan115219@gmail.com>
2026-01-22 13:45:24 +0600

    changes for testing

--- redirect.js
+++ redirect.js
@@ -1,25 +1,25 @@
-// // Define valid routes
-// const routes = [
-//   "/", // Home
-//   "/index.html",
-//   "/pages/about.html", // About Us
-//   "/pages/service.html", // All Services
-//   "/pages/web-development-services.html", // Web Development
-//   "/pages/app-development-services.html", // App Development
-//   "/pages/projects.html", // Portfolio
-//   "/pages/contact.html", // Contact
-//   "/pages/thank-you.html",
-//   "/pages/erp_solution.html",
-// ];
+// Define valid routes
+const routes = [
+  "/", // Home
+  "/index.html",
+  "/pages/about.html", // About Us
+  "/pages/service.html", // All Services
+  "/pages/web-development-services.html", // Web Development
+  "/pages/app-development-services.html", // App Development
+  "/pages/projects.html", // Portfolio
+  "/pages/contact.html", // Contact
+  "/pages/thank-you.html",
+  "/pages/erp_solution.html",
+];
 
-// // Get the current route
-// const path = window.location.pathname;
-// console.log("Current Path:", path);
+// Get the current route
+const path = window.location.pathname;
+console.log("Current Path:", path);
 
-// // Check if the path is valid
-// if (!routes.includes(path)) {
-//   console.log("Invalid path detected. Redirecting to 404...");
-//   window.location.href = "/404.html";
-// } else {
-//   console.log("Valid path:", path);
-// }
+// Check if the path is valid
+if (!routes.includes(path)) {
+  console.log("Invalid path detected. Redirecting to 404...");
+  window.location.href = "/404.html";
+} else {
+  console.log("Valid path:", path);
+}
